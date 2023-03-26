fe9f88d8415cf5b6c4b7d43330fd7f49124179d8
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-03-26 10:26:50 +0530

    Update main.py

--- Stress-Level-Detector/main.py
+++ Stress-Level-Detector/main.py
@@ -34,7 +34,7 @@ st.sidebar.title("Navigation")
 page = st.sidebar.radio("Pages", list(Tabs.keys()))
 
 # Loading the dataset.
-df, X, y = load_data('Stress.csv')
+df, X, y = load_data()
 
 # Call the app funciton of selected page to run
 if page in ["Prediction", "Visualisation"]:
