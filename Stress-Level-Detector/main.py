6e5111f77dfe14b7dd61163aebd27dfa3ea3998d
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-03-26 10:50:23 +0530

    Update main.py

--- Stress-Level-Detector/main.py
+++ Stress-Level-Detector/main.py
@@ -8,55 +8,6 @@ from web_functions import load_data
 
 # Import pages
 from Tabs import home, data, predict, visualise
-import numpy as np
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-import streamlit as st
-df, X, y = load_data()
-@st.cache()
-def load_data():
-    """This function returns the preprocessed data"""
-
-    # Load the Diabetes dataset into DataFrame.
-    df = pd.read_csv('Stress.csv')
-
-    # Rename the column names in the DataFrame.
-    df.rename(columns = {"t": "bt",}, inplace = True)
-    
-    
-
-    # Perform feature and target split
-    X = df[["sr","rr","bt","lm","bo","rem","sh","hr"]]
-    y = df['sl']
-
-    return df, X, y
-
-@st.cache()
-def train_model(X, y):
-    """This function trains the model and return the model and model score"""
-    # Create the model
-    model = DecisionTreeClassifier(
-            ccp_alpha=0.0, class_weight=None, criterion='entropy',
-            max_depth=4, max_features=None, max_leaf_nodes=None,
-            min_impurity_decrease=0.0, min_samples_leaf=1, 
-            min_samples_split=2, min_weight_fraction_leaf=0.0,
-            random_state=42, splitter='best'
-        )
-    # Fit the data on model
-    model.fit(X, y)
-    # Get the model score
-    score = model.score(X, y)
-
-    # Return the values
-    return model, score
-
-def predict(X, y, features):
-    # Get model and model score
-    model, score = train_model(X, y)
-    # Predict the value
-    prediction = model.predict(np.array(features).reshape(1, -1))
-
-    return prediction, score
 
 # Configure the app
 st.set_page_config(
@@ -83,6 +34,7 @@ st.sidebar.title("Navigation")
 page = st.sidebar.radio("Pages", list(Tabs.keys()))
 
 # Loading the dataset.
+df,X,Y=load_data()
 
 
 # Call the app funciton of selected page to run
